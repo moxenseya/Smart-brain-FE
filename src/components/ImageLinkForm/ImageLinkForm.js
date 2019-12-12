284f73aa597ee5deb0488c2556e3c21e620dce30
Abrar Hussain Syed <keyblademaster1000@gmail.com>
2019-12-12 15:47:33 -0500

    Added sample image URL for assisting

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,8 +5,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Web App detects faces and awards you one score point per face image provided. Contribute to the Hive Mind and be one with the machine!'}
+        {'This Web App detects faces. Contribute to the Hive Mind and be one with the machine!'}
       </p>
+      <p><i>Like this URL: https://avatars0.githubusercontent.com/u/29281629</i></p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
